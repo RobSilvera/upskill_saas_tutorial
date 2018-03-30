f9541336874ab7f41b09d98dda71b49e21f1eb7f
Rob Silvera <silvera@protonmail.com>
2018-03-30 01:28:46 +0000

    add create action to form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,22 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
     
   end  
+  
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: "Have a Blessed Day!"
+    else
+      redirect_to new_contact_path, notice: "Uh-oh An error occured!"
+    end
+  end
+  
+  private
+  
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    
+    end
 end
