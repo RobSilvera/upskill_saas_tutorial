1f52922afec4b7344bb920cbd2eeb69c81c89cb1
Rob Silvera <silvera@protonmail.com>
2018-04-12 17:31:57 +0000

    Mailer View

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,10 @@
-class ContactMailer< ActionMailer::Base
-    default to: 'chundo@dogvox.com'
-    def contact_email (name, email, body)
-      @name = name
-      @email = email
-      @body = body
-      mail(from: email, subject: "Dog Match Contact")
-    end
+class ContactMailer < ActionMailer::Base
+  default to: 'chundo@dogvox.com'
+    
+  def contact_email(name, email, comments)
+    @name = name
+    @email = email
+    @comments = comments
+    mail(from: email, subject: 'Dog Match Contact')
+  end
 end
