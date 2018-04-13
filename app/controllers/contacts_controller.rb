99dce46e7ad56f5aa680cd4f7273e4e05702bc55
Rob Silvera <silvera@protonmail.com>
2018-04-13 19:12:11 +0000

    Comments added

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,21 +1,30 @@
 class ContactsController < ApplicationController
+  
+  # GET request to /contact-us
+  # Show new contact form
   def new
     @contact = Contact.new
     
   end  
   
+  # POST request /contacts
   def create
+    
+    # Mass assignment of form fields into Contact object
     @contact = Contact.new(contact_params)
+    
+    #Save Conject to dataase
     if @contact.save
-      
-      
-      
+      # Store form fields via params into variables
       name = params[:contact] [:name]
       email = params[:contact] [:email]
       comments = params[:contact] [:comments]
+      
+      # Injects variables into ContactMailer 
+      # mail method and send email
       ContactMailer.contact_email(name, email, comments).deliver
       
-      
+      # Relaods contact page with message
       flash[:success] = "Have a Blessed Day!"
       redirect_to new_contact_path
     else
@@ -25,7 +34,8 @@ class ContactsController < ApplicationController
   end
   
   private
-  
+    # To collect data from forms, we need to use
+    # strong parameters and whitelist the form fields
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
     
